9e1df1cd64a7b0693604cf220b4acdfe898f89e2
LacoTuranyi <ladislav.turanyi@icloud.com>
2020-04-21 19:56:06 +0000

    Added about pageview action  and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   #resources :posts
+  
+  get '/about' => 'pages#about'
   root 'pages#home'
   #root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
